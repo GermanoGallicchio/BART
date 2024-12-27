0bec0d19e942758e9a2d1bd671346b46eff58c64
GermanoGallicchio <germano.gallicchio@gmail.com>
2024-12-27 18:40:42 +0000

    Initial commit

---
+++ temp.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+
+# number of balloons (i.e., trials) (128 in the original blue-balloon study)
+# this is also the hghest number of pumps permitted overall 
+nTrials = 10
+
+maxPumps = np.arange(nTrials)+1
+np.random.shuffle(maxPumps) # permute the order
+
+np.round(0.221321, 2)
+
+
